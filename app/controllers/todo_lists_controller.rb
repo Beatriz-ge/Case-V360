b978d84cdd04d1a050f970436edf0ce302e5a695
Beatriz Lins <bibi.geane2@gmail.com>
2026-01-16 20:57:53 -0300

    feat: criação dos controllers TasksController e TodoListsController

---
+++ app/controllers/todo_lists_controller.rb
@@ -0,0 +1,29 @@
+class TodoListsController < ApplicationController
+  def index
+    @lists = TodoList.all
+  end
+
+  def show
+    @list = TodoList.find(params[:id])
+    @task = Task.new
+  end
+
+  def new
+    @list = TodoList.new
+  end
+
+  def create
+    @list = TodoList.new(list_params)
+    if @list.save
+      redirect_to @list
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:todo_list).permit(:title)
+  end
+end
